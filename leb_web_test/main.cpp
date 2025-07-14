512f22c60008cb027a33c73f830c5506cb87d636
NickDos <dosremediosn@wit.edu>
2025-07-13 22:23:07 -0400

    testing and comments

--- leb_web_test/main.cpp
+++ leb_web_test/main.cpp
@@ -9,19 +9,19 @@ int main(int argc, char** argv)
 	int exit = sqlite3_open(db_path, &DB);
 
 
-	//string email;
-	//cout << "Enter email: ";
-	//cin >> email;
-	//string password;
-	//cout << "Enter password: ";
-	//cin >> password;
-	//while (user_login(DB, db_path, email, password) == 0) {
-	//	cout << "Incorrect Email or Password" << endl;
-	//	cout << "Enter email: ";
-	//	cin >> email;
-	//	cout << "Enter password: ";
-	//	cin >> password;
-	//}
+	string email;
+	cout << "Enter email: ";
+	cin >> email;
+	string password;
+	cout << "Enter password: ";
+	cin >> password;
+	while (user_login(DB, db_path, email, password) == 0) {
+		cout << "Incorrect Email or Password" << endl;
+		cout << "Enter email: ";
+		cin >> email;
+		cout << "Enter password: ";
+		cin >> password;
+	}
 
 	/*string email = "whited3";
 	string password = "Tp8P!9Ls";*/
@@ -29,8 +29,8 @@ int main(int argc, char** argv)
 	//string email = "curiem";
 	//string password = "Uz7V^5Mw";
 
-	string email = "brooksm";
-	string password = "qW9t@3zL";
+	/*string email = "brooksm";
+	string password = "qW9t@3zL";*/
 
 	/*student* tempStud = new student();
 	populate_student(DB, db_path, tempStud, "curiem");
@@ -57,7 +57,7 @@ int main(int argc, char** argv)
 		print_admin();
 		int user_in;
 		cin >> user_in;
-		while (logout(user_in) != 0) {
+		while (logout(user_in) != 0) {					//Nick
 			if (user_in == 1) {
 				testUser->add_courses(DB);
 			}
@@ -94,7 +94,7 @@ int main(int argc, char** argv)
 			print_admin();
 			cin >> user_in;
 		}
-	} else if (get_role(DB, db_path, email) == "Instructor") {
+	} else if (get_role(DB, db_path, email) == "Instructor") {		//Nick 
 		instructor* testUser = new instructor();
 		populate_instructor(DB, db_path, testUser, email);
 		cout << "Welcome " << testUser ->show_first_name() << endl;
@@ -128,7 +128,7 @@ int main(int argc, char** argv)
 			print_instructor();
 			cin >> user_in;
 		}
-	} else if (get_role(DB, db_path, email) == "Student") {
+	} else if (get_role(DB, db_path, email) == "Student") {		//Robbie
 		student* testUser = new student();
 		populate_student(DB, db_path, testUser, email);
 		testUser->setSchedule(DB, db_path);
